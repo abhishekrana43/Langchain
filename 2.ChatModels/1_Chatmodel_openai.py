3cf66da388a36b820361c247a96c07e2aaa34494
Abhishek Rana <abhishekrana0743@gmail.com>
2026-03-06 23:01:38 +0530

    Add thr OPENAI model

---
+++ 2.ChatModels/1_Chatmodel_openai.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model=ChatOpenAI(model='gpt-4', temperature=0)
+
+result=model.invoke('Write a 5 lines poem on cricket')
+
+print(result.content)
