3187983e59f9d7bdfea1ada6aaba578f93bd86c6
Abhishek Rana <abhishekrana0743@gmail.com>
2026-03-20 22:23:06 +0530

    PDF Loader in langchain

---
+++ 9.Document_Loader/9.2.Pdf_loader.py
@@ -0,0 +1,13 @@
+from langchain_community.document_loaders import PyPDFLoader
+from dotenv import load_dotenv
+
+load_dotenv()
+
+loader = PyPDFLoader('dl-curriculum.pdf')
+
+docs = loader.load()
+
+print(len(docs))
+
+print(docs[0].page_content)
+print(docs[1].metadata)
